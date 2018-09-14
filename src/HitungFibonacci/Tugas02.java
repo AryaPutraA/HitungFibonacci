ce9a5c67525132ec501ffc3c3769a953bef8d828
ASUS <ASUS@Pulu-Pulu>
2018-09-14 19:54:31 +0700

    Tugas2 J07 Input

---
+++ src/HitungFibonacci/Tugas02.java
@@ -0,0 +1,18 @@
+package HitungFibonacci;
+import java.util.Scanner;
+public class Tugas02 
+{
+	
+    
+	private static int Awok()
+	{
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Berat Barang : ");
+        int berat = scan.nextInt();
+        
+        return berat;
+	}
+    
+	
+}
