90b3e6eecb6b1592f5d4f22de25a02587ffa305a
ASUS <ASUS@Pulu-Pulu>
2018-09-14 19:40:57 +0700

    Tugas1 J07 Input

---
+++ src/HitungFibonacci/Tugas01.java
@@ -0,0 +1,19 @@
+package HitungFibonacci;
+import java.util.Scanner;
+
+public class Tugas01 
+{
+		
+		
+		public static int input()
+		{
+			Scanner o = new Scanner(System.in);
+			
+			System.out.print("Masukan Jumlah Uang : Rp.");
+			int b = o.nextInt();
+			
+			return b;
+		}
+		
+		
+}
