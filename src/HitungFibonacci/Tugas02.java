f6b9e7474e72abe633309d7d31fde6b6ec741f28
ASUS <ASUS@Pulu-Pulu>
2018-09-14 19:55:59 +0700

    Tugas2 J07 Proses

--- src/HitungFibonacci/Tugas02.java
+++ src/HitungFibonacci/Tugas02.java
@@ -14,5 +14,23 @@ public class Tugas02
         return berat;
 	}
     
-	
+	private static void Proces(int berat, int[] ap, int[] pa)
+	{
+		int hasil = 0;
+		for (int i = 0; i < ap.length; i++) 
+		{
+			int n = 0;
+			while(berat>=ap[i])
+			{
+				berat = berat- ap[i];
+				n++;
+			}
+		int total = n*pa[i];
+		System.out.println(ap[i] + " Sebanyak " + n + " dengan harga Rp" + total);
+		hasil = hasil + total;
+		}
+        
+        System.out.println("\nTotal Bayaran : Rp." + hasil);
+	}
+
 }
